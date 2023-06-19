e272c232e97619d2d268d3c7c5fdf219200ed099
Alvin Rachmat <80054848+alvinrach@users.noreply.github.com>
2023-06-19 21:24:59 +0700

    Add km buttons-behavior like radio button

--- app.py
+++ app.py
@@ -31,7 +31,34 @@ transportations = st.selectbox(
     ('Motorcycle', 'Car', 'Taxi', 'Bus', 'Train', 'MRT'))
 transportations = transportations.lower()
 
-km = st.number_input('How long is that in kilometer?', step=1)
+if 'km' not in st.session_state:
+    st.session_state['km'] = 1
+
+km = st.number_input('How long is that in kilometer?', step=1, value=st.session_state['km'], key='km_input')
+
+col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+
+with col1:
+    if st.button('1 Km', key='1_km_button', on_click=lambda: st.session_state.update({'km': 1})):
+        st.session_state['km'] = 1
+with col2:
+    if st.button('2 Km', key='2_km_button', on_click=lambda: st.session_state.update({'km': 2})):
+        st.session_state['km'] = 2
+with col3:
+    if st.button('5 Km', key='5_km_button', on_click=lambda: st.session_state.update({'km': 5})):
+        st.session_state['km'] = 5
+with col4:
+    if st.button('10 Km', key='10_km_button', on_click=lambda: st.session_state.update({'km': 10})):
+        st.session_state['km'] = 10
+with col5:
+    if st.button('20 Km', key='20_km_button', on_click=lambda: st.session_state.update({'km': 20})):
+        st.session_state['km'] = 20
+with col6:
+    if st.button('50 Km', key='50_km_button', on_click=lambda: st.session_state.update({'km': 50})):
+        st.session_state['km'] = 50
+with col7:
+    if st.button('100 Km', key='100_km_button', on_click=lambda: st.session_state.update({'km': 100})):
+        st.session_state['km'] = 100
 
 co2total = round(gco2e[transportations]*km, 1)
 
